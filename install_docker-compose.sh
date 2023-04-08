c48d784dd6655a0d6d7acdc8c0906cee0b680766
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-08 14:43:23 -0400

    Create install_docker-compose.sh

---
+++ install_docker-compose.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Check to see if Docker Compose is installed on the system, if it isn't
+# install it, if its already present on the system, notify the user and exit.
+# This script will also install the latest version of docker-compose over an
+# existing version of docker-compose.
+
+# Works Cited [COMPOSE_VERSION]: Thank you Roberto Rodriguez (https://github.com/Cyb3rWard0g)
+function docker_compose_install() {
+
+   INSTALL_DIR=/usr/local/bin
+   DC_LOC=`which docker-compose`
+   DC_CURVER=`docker-compose version | awk '{ print $4 }'`
+   COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+   Color_Off='\033[0m'
+   Green='\033[0;32m'
+
+   if [ -f "$DC_LOC" ] && [ "$COMPOSE_VERSION" = "$DC_CURVER" ]; then
+      echo -e "`docker-compose version` exists and is the most current version. Exiting."
+      exit
+   else
+      if [ "$COMPOSE_VERSION" != "$DC_CURVER" ]; then
+          echo -e "$Green Installing a newer version of docker-compose..$Color_Off"
+          sudo rm -rf $(DC_LOC)
+      fi
+
+      sudo curl -SL https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` \
+      -o $INSTALL_DIR/docker-compose
+
+      echo "Docker Compose successfully downloaded to: $INSTALL_DIR"
+
+      sudo chmod 755 $INSTALL_DIR/docker-compose
+      sudo ln -s $INSTALL_DIR/docker-compose /usr/bin/docker-compose
+
+      echo "Docker Compose successfully linked to: /usr/bin/docker-compose"
+      echo "`docker-compose version` has been successfully installed on this system."
+   fi
+}
+
+docker_compose_install
